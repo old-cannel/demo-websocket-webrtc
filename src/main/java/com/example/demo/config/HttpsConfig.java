aa6bdcd14daaeb04a2e2180e05a6080f3b6142ec
sunnyliuqi <liu963277983>
2019-01-28 16:19:34 +0800

    https搭建完成

---
+++ src/main/java/com/example/demo/config/HttpsConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther: liuqi
+ * @Date: 2019/1/28 13:46
+ * @Description: Https 配置类
+ */
+@Configuration
+public class HttpsConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(0);
+        return connector;
+    }
+}
